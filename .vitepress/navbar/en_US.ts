4517cc2a766143d2a3a632d8afde6cd9b9f16b06
BlockHaity <104670904+BlockHaity@users.noreply.github.com>
2025-08-23 22:20:10 +0800

    Fix: update navigation links and redirect page

--- .vitepress/navbar/en_US.ts
+++ .vitepress/navbar/en_US.ts
@@ -1,6 +1,6 @@
 import { DefaultTheme } from 'vitepress'
 
 export const enNav: DefaultTheme.NavItem[] = [
-  { text: 'Home Page', link: '/' },
+  { text: 'Home Page', link: '/en_US/' },
   { text: 'Download', link: '/en_US/download/', activeMatch: '/en_US/download/' },
 ]
--- src/index.md
+++ src/index.md
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
 <head>
     <meta http-equiv="refresh" content="0;url=/zh_CN">
 </head>
-<body>
-    <p>页面将自动跳转到<a href="/zh_CN">/zh_CN</a></p>
-</body>
-</html>
+
+页面将自动跳转到[/zh_CN](/zh_CN)
